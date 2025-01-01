9f3b5f420e4f4e4e5ed222bac68fb82b1e154162
ronzzz837 <ronzzyt22@gmail.com>
2025-01-01 18:49:39 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const createError = require("http-errors");
+const cors = require("cors");
+const secure = require("ssl-express-www");
+const cookieParser = require("cookie-parser");
+
+const api = require("./api");
+
+const app = express();
+
+app.set("trust proxy", true);
+app.set("json spaces", 2);
+app.use(cors());
+app.use(secure);
+app.use(cookieParser());
+app.use("/", api);
+
+app.use(function (req, res, next) {
+  next(createError(404))
+});
+
+app.use(function (err, req, res, next) {
+  res.json({
+    status: false,
+    creator: "Ronzz YT",
+    message: "No akses!!!"
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Aktif!")
+});
+
+module.exports = app
